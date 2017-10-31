256707eee1e98f61ad484e64e7488e69a2ae9f11
fawda123 <mbafs2012@gmail.com>
2017-10-31 13:27:01 -0700

    matlab data processed

---
+++ R/dat_proc.R
@@ -0,0 +1,41 @@
+library(R.matlab)
+library(tidyverse)
+library(tibble)
+
+raw <- readMat('raw/DataForNina.mat')
+
+##
+# crab data
+
+# names of crab variables, unknown might be 'night'
+crnm <- c('CTD', 'unk', 'abundances', 'Average CL', 'Average CW', 
+          'Average R-DCS', 'Average TL', 'pa')
+
+crbs <- raw$Crabs %>% 
+  as.tibble %>% 
+  rename_all(funs(make.names(crnm)))
+
+save(crbs, file = 'data/crbs.RData', compress = 'xz')
+
+##
+# variable names
+
+vrnm <- raw$VarList %>% 
+  unlist %>% 
+  c('depth', 'CTD', .)
+
+##
+# environmental data
+
+# convert array to lis
+envdat <- raw$Data %>% 
+  array_tree(margin = 1) %>% 
+  enframe('depth') %>% 
+  mutate(
+    depth = 10 * depth, 
+    value = map(value, ~ data.frame(CTD = crbs$CTD, .))
+  ) %>% 
+  unnest %>% 
+  rename_all(funs(c(vrnm)))
+
+save(envdat, file = 'data/envdat.RData', compress = 'xz')
