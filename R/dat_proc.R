ff000eb6c2e4bdafbd90328fabc4e0766902340c
fawda123 <mbafs2012@gmail.com>
2018-12-13 11:07:16 -0800

    processed seperate dissolution, length data for disseval.Rmd

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,3 +1,6 @@
+######
+# for crab_explr.Rmd, results.Rnw
+
 library(R.matlab)
 library(tidyverse)
 library(readxl)
@@ -68,3 +71,57 @@ envdat <- plyr::rbind.fill(envdat, mandat) %>%
   arrange(depth, CTD)
   
 save(envdat, file = 'data/envdat.RData', compress = 'xz')
+
+######
+# for disseval.Rmd
+
+library(tidyverse)
+library(readxl)
+
+##
+# dissolution data
+dissdat <- read_excel('raw/Copy of SEM crab scoring sheet_renewed analyses_finalised.xlsx', skip = 2)
+nms <- names(dissdat)
+nms <- case_when(
+  nms %in% c('X__3') ~ 'diss_pore',
+  nms %in% c('X__6') ~ 'diss_ridg', 
+  nms %in% c('X__9') ~ 'diss_crys', 
+  nms == 'CTD station' ~ 'CTD',
+  nms == 'Body part' ~ 'prt',
+  T ~ nms
+)
+names(dissdat) <- nms
+dissdat <- dissdat %>% 
+  select(CTD, prt, diss_pore, diss_ridg, diss_crys) %>% 
+  .[-1, ] %>% 
+  fill_('CTD') %>% 
+  mutate(prt = gsub('^Body part$', 'body part', prt)) %>% 
+  mutate_if(!names(.) %in% 'prt', as.numeric) %>% 
+  gather('distyp', 'disval', -CTD, -prt)
+
+save(dissdat, file = 'data/dissdat.RData', compress = 'xz')
+
+##
+# length data, all
+lengdatall <- read_excel('raw/Long deliniated lengths.xlsx') %>% 
+  rename(
+    shoreloc = `onshore = 2; offshore 1`
+  ) %>% 
+  mutate(
+    shoreloc = case_when(
+      shoreloc == 2 ~ 'onshore', 
+      shoreloc == 1 ~ 'offshore'
+    ),
+    CL = ifelse(CL > 9, NA, CL)
+  )
+
+# length dat, summarized
+lengdat <- lengdatall %>% 
+  gather('lenvar', 'lenval', -CTD, -shoreloc) %>%
+  group_by(CTD, shoreloc, lenvar) %>%
+  summarise(
+    avelen = mean(lenval, na.rm = T),
+    stelen = 1.96 * sd(lenval, na.rm = T)/(length(na.omit(lenval)))
+  )
+
+save(lengdat, file = 'data/lengdat.RData', compress = 'xz')
